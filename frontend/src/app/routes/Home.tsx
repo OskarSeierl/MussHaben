388acec435308f2e030760d063a511ff8da2ff8f
Oskar <oskar.seierl@aon.at>
2026-02-23 18:42:17 +0100

    feat: remove test button

--- frontend/src/app/routes/Home.tsx
+++ frontend/src/app/routes/Home.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import {Button, Stack, Typography} from "@mui/material";
+import {Stack, Typography} from "@mui/material";
 import {useAuth} from "../../hooks/useAuth.ts";
 import {AllSearchAgents} from "../../components/search-agents/AllSearchAgents.tsx";
 import {AllSearchAgentsHeadline} from "../../components/search-agents/AllSearchAgentsHeadline.tsx";
-import {getListings} from "../../config/api.ts";
 
 const Home: React.FC = () => {
     const {user} = useAuth();
@@ -13,10 +12,6 @@ const Home: React.FC = () => {
             <Typography variant="h4" gutterBottom>Hallo {user?.displayName || user?.email || ""}</Typography>
             <AllSearchAgentsHeadline variant={"h5"} />
             <AllSearchAgents/>
-            <Button onClick={async () => {
-                const result = await getListings();
-                console.log(result.data);
-            }}>Click to get listings</Button>
         </Stack>
     );
 };
